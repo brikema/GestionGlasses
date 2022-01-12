6b0febc43a68e64240301614339e6c3420f48aed
brikema <brice_ma@hotmail.fr>
2022-01-12 14:12:58 +0100

    Init du projet

---
+++ src/test/java/eu/ensup/gestionglasses/GestionglassesApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.ensup.gestionglasses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionglassesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
